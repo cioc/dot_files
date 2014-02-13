5b67d34cb25c733394355325651d1087b892e915
Charles Cary <cclego@gmail.com>
2014-02-13 05:22:58 +0000

    java packages

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ apt-get -y install make
 apt-get -y install mit-scheme
 apt-get -y install vim
 apt-get -y install python-dev
+apt-get -y install openjdk-7-jre
+apt-get -y install openjdk-7-jdk
 cp .vimrc ~/.vimrc
