81533b8cb938014d53a4c445d72dca96f470a751
Charles Cary <charlescary@Charless-MacBook-Air.local>
2014-03-08 18:05:50 -0800

    npm

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ apt-get -y install vim
 apt-get -y install python-dev
 apt-get -y install openjdk-7-jre
 apt-get -y install openjdk-7-jdk
+apt-get -y install curl
+curl https://npmjs.org/install.sh | sh
 cp .vimrc ~/.vimrc
